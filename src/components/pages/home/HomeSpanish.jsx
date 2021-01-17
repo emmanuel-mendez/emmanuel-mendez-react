5239dce3fae9c80e49088fed59b68ec26fcbea61
emmanelos <emmanuelm132911.com>
2021-01-17 05:02:38 -0400

    Footer fixed

---
+++ src/components/pages/home/HomeSpanish.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export const HomeSpanish = () => {
+	return (
+		<article className="article">
+
+			<div className="article__container">
+
+				<section className="section__description">
+
+					<div className="section__descriptionImage"></div>
+
+					<div className="section__descriptionText">
+
+						<h2 className="section__descriptionTitle">EMMANUEL FABIÁN MÉNDEZ MARTINEZ</h2>
+
+						<p className="section__descriptionParagraph">Estudiante en la Universidad Nacional Experimental de la Gran Caracas</p>
+
+						<p className="section__descriptionParagraph">Web developer</p>
+
+						<p className="section__descriptionParagraph">Nacionalidad: Venezolano</p>
+
+						<p className="section__descriptionParagraph">Languajes: English / Spanish</p>
+
+					</div>
+
+				</section>
+
+			</div>
+
+		</article>
+	)
+}
