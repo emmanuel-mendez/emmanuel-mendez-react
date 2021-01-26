149f312185af4d5c0a293cfa1a42326f89535380
emmanelos <emmanuelm132911.com>
2021-01-26 15:20:37 -0400

    skills

--- src/components/routes/home/Home.jsx
+++ src/components/routes/home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react'
-// import { useLang } from '../../hooks/useLang'
+import { useLang } from '../../hooks/useLang'
 
 import { ReactComponent as HomeImage } from '../../../svg/homeImage.svg';
 
@@ -16,18 +16,42 @@ const Home = () => {
 				<section className="home">
 
 					<div className="home__description">
-						<p className="home__introduce">Hi, my name is</p>
+						<p className="home__introduce">
+							{
+								useLang()
+									? "Hola, mi nombre es"
+									: "Hi, my name is"
+							}
+						</p>
 
 						<h2 className="home__title">Emmanuel Méndez.</h2>
 
-						<h3 className="home__subtitle">I'm a web developer.</h3>
+						<h3 className="home__subtitle">
+							{
+								useLang()
+									? "Soy un desarrollador web."
+									: "I'm a web developer."
+							}
+						</h3>
 
-						<p> I specialize in designing and building exceptional websites, databases and cross-platform applications</p>
+						<p className="home__paragraph">
+							{
+								useLang()
+									? "Me especializo en el diseño de sitios web excepcionales, desarrollo de aplicaciones multiplataforma y la administración bases de datos."
+									: "I specialize in designing exceptional websites, building cross-platform applications and manage databases."
+							}
+						</p>
+
+						<div className="home__getInTouchButton">
+							{
+								useLang()
+									? "Contáctame"
+									: "Get in touch"
+							}
+						</div>
 					</div>
 
-					<button className="home__getInTouchButton">
-						Get in touch
-					</button>
+
 
 					<HomeImage className="home__image" />
 
@@ -38,11 +62,26 @@ const Home = () => {
 
 					<div className="aboutMe__description">
 
-						<h2 className="aboutMe__title">About me.</h2>
+						<h2 className="aboutMe__title">
+							{
+								useLang()
+									? "Acerca de mi."
+									: "About me."
+							}
+						</h2>
 
-						<p className="aboutMe__introduce">Hello, I'm Emmanuel Méndez, a web developer based in Caracas, Venezuela</p>
+						<p className="aboutMe__introduce">
+							{useLang()
+								? "Hola, soy Emmanuel Méndez, un desarrollador web residente en Caracas, Venezuela"
+								: "Hello, I'm Emmanuel Méndez, a web developer based in Caracas, Venezuela"
+							}
+						</p>
 
-						<p>I'm a student at<span> Universidad Nacional Experimental de la Gran Caracas</span> that love create landing pages, single-page applications, ecommerce, progressive web apps, SQL and NoSQL databases, REST APISs and GraphQL APIs</p>
+						<p>
+							{useLang()
+								? <span>Soy un estudiante de la <a className="aboutMe__unexca" href="http://www.unexca.edu.ve/">Universidad Nacional Experimental de la Gran Caracas</a> que ama crear landing pages, single-page applications, ecommerce, progressive web apps, bases de datos SQL y NoSQL,  APISs REST y APIs GraphQL</span>
+								: <span>I'm a student at <a className="aboutMe__unexca" href="http://www.unexca.edu.ve/">Universidad Nacional Experimental de la Gran Caracas</a> that love create landing pages, single-page applications, ecommerce, progressive web apps, SQL and NoSQL databases, REST APISs and GraphQL APIs</span>
+							}</p>
 
 					</div>
 
--- src/components/routes/skills/Skills.jsx
+++ src/components/routes/skills/Skills.jsx
@@ -1,75 +1,249 @@
 import React from 'react'
 
+import { useLang } from '../../hooks/useLang'
+
+import { ReactComponent as SkillsImage } from '../../../svg/skillsImage.svg';
+
+import { ReactComponent as Frontend } from '../../../svg/frontend.svg';
+
+import { ReactComponent as Backend } from '../../../svg/backend.svg';
+
 const Skills = () => {
 	return (
 		<article className="article">
 
 			<div className="route">
 
-				<section className="section__skills">
+				<section className="skills">
 
-					<h2 className="section__skillsTitle">SKILLS</h2>
+					<div className="skills__description">
+						<h2 className="skills__title">{
+							useLang()
+								? "Habilidades."
+								: "Skills."
+						}</h2>
 
-					<div>
-						<h3>Markup Languages</h3>
-						<p>HTML</p>
+						<ul className="skills__advantages">
+							<li className="skills__advantage">
+								{
+									useLang()
+										? "Domino todas las técnicas involucradas en el desarrollo de un proyecto."
+										: "Master all the techniques involved in a development project."
+								}
+							</li>
+
+							<li className="skills__advantage">
+								{
+									useLang()
+										? "Realizo prototipos en tiempo record."
+										: "Make prototypes very rapidly."
+								}
+							</li>
+
+							<li className="skills__advantage">
+								{
+									useLang()
+										? "Brindo ayuda a todos los miembros del equipo."
+										: "Provide help to all the team members."
+								}
+							</li>
+
+							<li className="skills__advantage">
+								{
+									useLang()
+										? "Reduzco el costo de los proyectos."
+										: "Reduce the cost of the projects."
+								}
+							</li>
+
+							<li className="skills__advantage">
+								{
+									useLang()
+										? "Cambio entre desarrollo frontend y backend, dependiendo de los requerimientos."
+										: "Switch between frontend and backend development based on requirements."
+								}
+							</li>
+
+							<li className="skills__advantage">
+								{
+									useLang()
+										? "Comprendo todos los aspectos de las nuevas tecnologías."
+										: "Understand all aspects of new technologies."
+								}
+							</li>
+						</ul>
+
 					</div>
 
-					<div>
-						<h3> Style Sheet Languages</h3>
-						<p>CSS</p>
+					<SkillsImage className="skills__image" />
 
-						<h4>Preprocessor Scripting Language</h4>
-						<p>Sass</p>
+				</section>
+
+				<section className="frontend">
+
+					<div className="frontend__description">
+
+						<h2 className="frontend__title">{
+							useLang()
+								? "Halibidades en frontend"
+								: "Frontend skills."
+						}</h2>
+
+						<ul className="frontend__skills">
+
+							<li className="frontend__skill">
+								<h3>{
+									useLang()
+										? "Lenguaje de marcado de hipertexto"
+										: "Markup Language"
+								}</h3>
+								<p>HTML</p>
+							</li>
+
+							<li className="frontend__skill">
+								<h3>{
+									useLang()
+										? "Hojas de estilo"
+										: "Style Sheet Language"
+								}</h3>
+								<p>CSS</p>
+							</li>
+
+							<li className="frontend__skill">
+								<h3>{
+									useLang()
+										? "Prepocesador de hojas de estilo"
+										: "Preprocessor Scripting Language"
+								}</h3>
+								<p>Sass</p>
+							</li>
+
+							<li className="frontend__skill">
+								<h3>CSS-in-JS</h3>
+								<p>Styled Components</p>
+								<p>Emotion</p>
+							</li>
+
+							<li className="frontend__skill">
+								<h3>{
+									useLang()
+										? "Lenuajes de programación"
+										: "Programming language"
+								}</h3>
+								<p>Javascript</p>
+								<p>Typescript</p>
+							</li>
+
+							<li className="frontend__skill">
+								<h3>{
+									useLang()
+										? "Librería"
+										: "Library"}</h3>
+								<p>React</p>
+							</li>
+
+							<li className="frontend__skill">
+								<h3>Framework</h3>
+								<p>Next.js</p>
+							</li>
+
+						</ul>
+
+
 					</div>
 
-					<div>
-						<h3>Programming languages</h3>
-						<div>
-							<h4>Javascript</h4>
+					<Frontend className="frontend__image" />
 
-							<h5>Backend</h5>
-							<p>Node.js</p>
-							<p>GraphQL</p>
 
-							<h5>Frontend</h5>
-							<p>React</p>
-						</div>
+				</section>
 
-						<div>
-							<h4>PHP</h4>
-						</div>
+				<section className="backend">
+
+					<div className="backend__description">
+
+						<h2 className="backend__title">{
+							useLang()
+								? "Habilidades en backend."
+								: "Backend skills."
+						}</h2>
+
+						<ul className="backend__skills">
+
+							<li className="backend__skill">
+								<h3>{
+									useLang()
+										? "Leguajes de programación"
+										: "Programming languages"
+								}</h3>
+								<p>Javascript</p>
+								<p>Typescript</p>
+								<p>PHP</p>
+								<p>C++</p>
+							</li>
+
+							<li className="backend__skill">
+								<h3>{
+									useLang()
+										? "Entorno de desarrollo para Javascript"
+										: "Runtime enviroment for Javascript"
+								}</h3>
+								<p>Node.js</p>
+							</li>
+
+							<li className="backend__skill">
+								<h3>{
+									useLang()
+										? "Entorno de desarrollo para Typescript"
+										: "Runtime enviroment for Typescript"
+								}</h3>
+								<p>Deno</p>
+							</li>
+
+							<li className="backend__skill">
+								<h3>APIs</h3>
+								<p>REST</p>
+								<p>GraphQL</p>
+							</li>
+
+							<li className="backend__skill">
+								<h3>{
+									useLang()
+										? "Servidor de GraphQL"
+										: "GraphQL server"
+								}</h3>
+								<p>Apollo server</p>
+							</li>
+
+							<li className="backend__skill">
+								<h3>{
+									useLang()
+										? "Bases de datos"
+										: "Databases"
+								}</h3>
+
+								<div className="backend__databases">
+									<h4>SQL</h4>
+									<p>MySQL</p>
+								</div>
+
+								<div className="backend__databases">
+									<h4>NoSQL</h4>
+									<p>MongoDB</p>
+								</div>
+							</li>
+
+						</ul>
+
 					</div>
 
-					<div>
-						<h3>Databases</h3>
-
-						<div>
-							<h4>SQL</h4>
-							<p>MySQL</p>
-						</div>
-
-						<div>
-							<h4>NoSQL</h4>
-							<p>MongoDB</p>
-						</div>
-					</div>
-
-					<div>
-						<h3>Distributed Version-control</h3>
-						<p>Git</p>
-					</div>
-
-					<div>
-						<h3>Others</h3>
-						<p>Responsive Web Desing</p>
-					</div>
+					<Backend className="backend__image" />
 
 				</section>
 
 			</div>
 
 		</article>
+
 	)
 }
 
