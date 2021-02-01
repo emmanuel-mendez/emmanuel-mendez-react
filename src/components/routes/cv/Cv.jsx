ec145a8b9b0f723898a6c0051935c472d2817a04
emmanelos <emmanuelm132911.com>
2021-01-31 22:24:23 -0400

    cv test

--- src/components/routes/cv/Cv.jsx
+++ src/components/routes/cv/Cv.jsx
@@ -8,6 +8,8 @@ const Cv = () => {
 		<>
 			<section className="cv">
 
+				<h2 className="cv__title">Curriculum Vitae.</h2>
+
 				<div className="cv__imgContainer">
 					{useLang()
 						? <img src={process.env.PUBLIC_URL + '/png/EMMANUEL-MÉNDEZ.png'} width="512" height="512" alt="Emmanuel Méndez" className="cv__img" />
@@ -16,15 +18,23 @@ const Cv = () => {
 				</div>
 
 				<div className="cv__links">
-					<h3 className="cv__title">Download</h3>
+					<h3 className="cv__linksTitle">Download</h3>
 
 					<ul className="cv__downloads">
 						<li className="cv__download">
-							PDF
+							{
+								useLang()
+									? <a href={process.env.PUBLIC_URL + '/pdf/EMMANUEL-MÉNDEZ.pdf'} target="blank">PDF</a>
+									: <a href={process.env.PUBLIC_URL + '/pdf/EMMANUEL-MENDEZ.pdf'} target="blank">PDF</a>
+							}
 						</li>
 
 						<li className="cv__download">
-							PNG
+							{
+								useLang()
+									? <a href={process.env.PUBLIC_URL + '/png/EMMANUEL-MÉNDEZ.png'} target="blank">PNG</a>
+									: <a href={process.env.PUBLIC_URL + '/png/EMMANUEL-MENDEZ.png'} target="blank">PNG</a>
+							}
 						</li>
 					</ul>
 				</div>
