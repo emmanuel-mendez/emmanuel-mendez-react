69f6ab3e21828f795fd84e4e734a73e4cff2238d
emmanelos <emmanuelm132911.com>
2021-02-01 23:22:46 -0400

    mm test

--- src/components/routes/contact/Contact.jsx
+++ src/components/routes/contact/Contact.jsx
@@ -12,46 +12,48 @@ const Contact = () => {
 	return (
 		<section className="contact">
 
-			<h2 className="contact__title">Contact.</h2>
+			<div className="contact__container">
+				<h2 className="contact__title">Contact.</h2>
 
-			<ul className="contact__links">
-				<li className="contact__linkContainer">
-					<a href="https://www.linkedin.com/in/emmanelos" target="blank" className="contact__link">
-						<Linkedin className="contact__linkLogo" />
-						<p>Linkedin</p>
-					</a>
-				</li>
+				<ul className="contact__links">
+					<li className="contact__linkContainer">
+						<a href="https://www.linkedin.com/in/emmanelos" target="blank" className="contact__link">
+							<Linkedin className="contact__linkLogo" />
+							<p>Linkedin</p>
+						</a>
+					</li>
 
-				<li className="contact__linkContainer">
-					<a href="https://www.facebook.com/emmanelos/" target="blank" className="contact__link">
-						<p>Facebook</p>
-						<Facebook className="contact__linkLogo" />
+					<li className="contact__linkContainer">
+						<a href="https://www.facebook.com/emmanelos/" target="blank" className="contact__link">
+							<p>Facebook</p>
+							<Facebook className="contact__linkLogo" />
 
-					</a>
-				</li>
+						</a>
+					</li>
 
-				<li className="contact__linkContainer">
-					<a href="https://www.instagram.com/emmanelos/" target="blank" className="contact__link">
-						<Instagram className="contact__linkLogo" />
-						<p>Instagram</p>
-					</a>
-				</li>
+					<li className="contact__linkContainer">
+						<a href="https://www.instagram.com/emmanelos/" target="blank" className="contact__link">
+							<Instagram className="contact__linkLogo" />
+							<p>Instagram</p>
+						</a>
+					</li>
 
-				<li className="contact__linkContainer">
-					<a href="https://twitter.com/emmanelost" target="blank" className="contact__link">
-						<p>Twitter</p>
-						<Twitter className="contact__linkLogo" />
+					<li className="contact__linkContainer">
+						<a href="https://twitter.com/emmanelost" target="blank" className="contact__link">
+							<p>Twitter</p>
+							<Twitter className="contact__linkLogo" />
 
-					</a>
-				</li>
+						</a>
+					</li>
 
-				<li className="contact__linkContainer">
-					<a href="https://github.com/emmanelos" target="blank" className="contact__link">
-						<Github className="contact__linkLogo" />
-						<p>Github</p>
-					</a>
-				</li>
-			</ul>
+					<li className="contact__linkContainer">
+						<a href="https://github.com/emmanelos" target="blank" className="contact__link">
+							<Github className="contact__linkLogo" />
+							<p>Github</p>
+						</a>
+					</li>
+				</ul>
+			</div>
 
 			<ContactImage className="contact__image" />
 
