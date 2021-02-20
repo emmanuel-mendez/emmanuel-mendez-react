33902fabbe383dba334ecf95a50eb05412e9f4b9
Emmanuel Méndez <emmanelosg@gmail.com>
2021-02-19 20:24:49 -0400

    offline png home

--- src/components/routes/home/AboutMeImage.jsx
+++ src/components/routes/home/AboutMeImage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function AboutMeImage() {
 	return (
-		<img src={process.env.PUBLIC_URL + '/emmanuelMendez.webp'} width="512" height="512" alt="Emmanuel Méndez" className="aboutMe__image" />
+		<img src={process.env.PUBLIC_URL + '/png/emmanuelMendez.png'} width="512" height="512" alt="Emmanuel Méndez" className="aboutMe__image" />
 	)
 }
 
